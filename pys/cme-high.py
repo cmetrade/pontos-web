0f6730c597e7e96b5b9dd45e96b73aadfdc28bd3
Andre Ricardo <cmetrade.6l@gmail.com>
2024-10-06 16:33:32 -0300

    1

--- pys/cme-high.py
+++ pys/cme-high.py
@@ -5,6 +5,8 @@ import os
 with open("arquivos/cme-low.txt", "r") as arquivo:
         x = arquivo.read()
 
+x = float(x.replace('.','').replace(',','.'))
+
 var1 = float(x)
 number = 1 / var1
 result = round(number, 4) 
@@ -13,4 +15,4 @@ result = round(number, 4)
 # Open the file in append mode
 with open('arquivos/cme-baixa.txt', 'w') as file:
     file.write(str(result))
-
+    print(result)
--- pys/cme-low.py
+++ pys/cme-low.py
@@ -4,6 +4,10 @@ import os
 
 with open("arquivos/cme-high.txt", "r") as arquivo:
         x = arquivo.read()
+        
+
+x = float(x.replace('.','').replace(',','.'))
+
 
 var1 = float(x)
 number = 1 / var1
@@ -13,4 +17,4 @@ result = round(number, 3)
 # Open the file in append mode
 with open('arquivos/cme-alta.txt', 'w') as file:
     file.write(str(result))
-
+print(result)
