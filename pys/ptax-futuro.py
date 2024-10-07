0c6b7eb349154cf8108ade5797a24506f8a7b932
Andre Ricardo <cmetrade.6l@gmail.com>
2024-10-07 20:33:12 -0300

    1

--- pys/ptax-futuro.py
+++ pys/ptax-futuro.py
@@ -3,7 +3,15 @@ import time
 import os
 
 os.system("awk {'print $1'} arquivos/ptax-futuro.txt > arquivos/ptax-futuro2.txt")
-with open("arquivos/ptax.txt", "r") as arquivo:
-        ptax1 = arquivo.read()
-with open("arquivos/ptax-futuro2.txt", "r") as arquivo:
-        ptax2 = arquivo.read()
+with open("arquivos/ptax.txt", "r") as ptax:
+        ptax1 = ptax.read()
+with open("arquivos/ptax-futuro2.txt", "r") as ptaxfut:
+        ptax2 = ptaxfut.read()
+with open('arquivos/ptax-ptaxfut.txt', 'w') as file:
+    file.write(str(ptax1))
+    file.write(' + ')
+with open('arquivos/ptax-ptaxfut.txt', 'a') as file2:
+    file2.write(str(ptax2))
+with open("arquivos/ptax-ptaxfut.txt", "r") as arquivo:
+        result = arquivo.read()
+print(result)
