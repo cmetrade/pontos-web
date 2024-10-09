0f5694d29f997e7743fe47a482964d967c5381b2
Andre Ricardo <cmetrade.6l@gmail.com>
2024-10-09 11:41:06 -0300

    1

--- shs/clear-cache.sh
+++ shs/clear-cache.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-aws cloudfront create-invalidation --distribution-id EI6WS8JUT1QQ7 --paths "/*"
+aws cloudfront create-invalidation --distribution-id EI6WS8JUT1QQ7 --paths "/pontos/index.html"
