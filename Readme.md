f7abccfc5cb6e26fcccad99c63a9a3fc6aa9be8f
Andre Ricardo <cmetrade.6l@gmail.com>
2024-10-16 18:45:06 -0300

    1

--- Readme.md
+++
@@ -1,9 +0,0 @@
-# Instalação da aplicação 
-# PARA INSTALAÇÃO DA APLICAÇÃO EXECUTE OS COMANDOS ABAIXO:
-
-sudo apt-get updade -y; 
-sudo apt-get install git -y;
-sudo apt-get install -y libgtk2.0-0 libgtk-3-0 libgbm-dev libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2 libxtst6 xauth xvfb -y; 
-git clone https://github.com/cmetrade/pontos; 
-chmod +x ~/pontos/shs/install-app.sh;
-~/pontos/shs/install-app.sh
---
+++ bkp/cme-abertura.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+cd ~/pontos-web
+mv cypress/e2e/* cypress/
+mv cypress/cme-abertura.cy.js cypress/e2e/
+
+i=1
+
+while [[ $i -lt 5 ]] 
+do
+        npm install
+        npm start
+        if [ "$?" == '0' ]
+           then
+           if [ -s "arquivos/cme-open.txt" ]; then
+                touch arquivos/cme-abertura.txt
+                python3 pys/cme-abertura.py
+           break
+           fi
+        fi
+echo "$i tentativa(s)"
+((i++))
+done
+
+echo "$i" >  logs/cme.log
